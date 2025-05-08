c927b40b5a852a6afb33f7bc83ab372e928b74e5
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-08 12:29:53 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,15 @@
+<?php
+ 
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Isotope SKU Search
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/isotope-sku-search
+ */
+
+/** Hooks */
+$GLOBALS['TL_HOOKS']['generatePage'][] 		 = array('Bcs\Hooks', 'generatePage');
+
+/* Models */
+//$GLOBALS['TL_MODELS']['tl_salsify_request']         = 'Bcs\Model\SalsifyRequest';
