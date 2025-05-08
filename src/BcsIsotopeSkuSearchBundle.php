2185e0e64c918ef1d551c4a716478a735335fbc4
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-08 11:27:04 -0400

    Create BcsIsotopeSkuSearchBundle.php

---
+++ src/BcsIsotopeSkuSearchBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Isotope SKU Search
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/isotope-sku-search
+ */
+
+namespace Bcs\IsotopeSkuSearchBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsIsotopeSkuSearchBundle extends Bundle
+{
+}
