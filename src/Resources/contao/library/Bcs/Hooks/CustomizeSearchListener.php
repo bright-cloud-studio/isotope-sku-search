ea16b4c6121cf3a7ceef36ad1780dbc96c1fd9f4
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-08 13:29:55 -0400

    Update CustomizeSearchListener.php

--- src/Resources/contao/library/Bcs/Hooks/CustomizeSearchListener.php
+++ src/Resources/contao/library/Bcs/Hooks/CustomizeSearchListener.php
@@ -4,6 +4,7 @@ namespace Bcs\Hooks;
 
 use Contao\Controller;
 use Contao\Database;
+use Contao\Input;
 use Contao\System;
 
 use Isotope\Interfaces\IsotopeProduct;
@@ -12,20 +13,18 @@ use Isotope\Model\Product;
 
 class CustomizeSearchListener
 {
-    public function onCustomizeSearch($arrPages, &$strKeywords, $strQueryType, $blnFuzzy)
-    {
-        $strKeywords .= "CHANGE MADE";
-    }
-
-
+    
     public function generatePage(&$objPageModel, $objLayout, &$objPage)
     {
+        // If, when loading a page, there is a keywords value in the URL, we are attempting a search!
+        if(strlen(Input::get('keywords'))) {
 
-        if($objPageModel->id == 249)
-        {
-           
+            // Try and find a product with the keyword as the SKU
+
+                // If we find a product, forward the user to that page
+            
         }
+        
     }
-
     
 }
