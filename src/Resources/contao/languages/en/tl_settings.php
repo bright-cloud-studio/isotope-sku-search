dc016a1b764ab9d81103e48123f90f7506d838bf
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-16 14:07:16 -0400

    Create tl_settings.php

---
+++ src/Resources/contao/languages/en/tl_settings.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_settings']['isotope_sku_search_legend']   = 'Isotope SKU Search Details';
+$GLOBALS['TL_LANG']['tl_settings']['produt_to_seek']              = array('Product to Seek', 'Select the Isotope Product we are looking for during our search, a variant or a parent');
